cbb9c0ff674a5892185cfe5d8445c93107ddb3bd
Ali <ghaddar.al@gmail.com>
2023-08-11 20:36:15 +0200

    Add MovieDetails.js and MovieDetails.css

--- src/App.js
+++ src/App.js
@@ -65,13 +65,13 @@ const App = () => {
 export default App ; 
 */
 
-
+/*
 import React, { useState, useEffect } from 'react';
 import MovieCard from './MovieCard';
 import './App.css';
 import SearchIcon from './search.svg';
 
-const API_URL = 'http://www.omdbapi.com?apikey=c0fa120';
+const API_URL = 'https://www.omdbapi.com?apikey=c0fa120';
 
 const App = () => {
   const [movies, setMovies] = useState([]);
@@ -121,23 +121,144 @@ const App = () => {
   );
 };
 
+export default App;*/
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////// use this:
+/*
+import React, { useState, useEffect } from 'react';
+import MovieCard from './MovieCard';
+import './App.css';
+import SearchIcon from './search.svg';
+
+const API_URL = 'https://www.omdbapi.com?apikey=c0fa120';
+
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    setMovies(data.Search || []);
+  };
+
+  useEffect(() => {
+    searchMovies(searchTerm);
+  }, [searchTerm]);
+
+  return (
+    <div className="app">
+      <h1>MovieVerse</h1>
+
+      <div className='search'>
+        <input
+          placeholder="Search for movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <img
+          src={SearchIcon}
+          alt='SearchIcon'
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard key={movie.imdbID} movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
+    </div>
+  );
+};
+
 export default App;
+*/
 
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////use lifo2
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import MovieCard from './MovieCard';
+import './App.css';
+import SearchIcon from './search.svg';
+import MovieDetails from './MovieDetails';
 
+const API_URL = 'https://www.omdbapi.com?apikey=c0fa120';
 
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
 
+    console.log('API Search Response:', data);
 
+    setMovies(data.Search || []);
+  };
 
+  useEffect(() => {
+    searchMovies(searchTerm);
+  }, [searchTerm]);
 
+  return (
+    <Router>
+      <div className="app">
+        <h1>MovieVerse</h1>
 
+        <div className="search">
+          <input
+            placeholder="Search for movies"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <img
+            src={SearchIcon}
+            alt="SearchIcon"
+            onClick={() => searchMovies(searchTerm)}
+          />
+        </div>
 
+        <Routes>
+          <Route path="/movie/:imdbID" element={<MovieDetails />} />
+          <Route
+            path="/"
+            element={
+              movies?.length > 0 ? (
+                <div className="container">
+                  {movies.map((movie) => (
+                    <Link to={`/movie/${movie.imdbID}`} key={movie.imdbID}>
+                      <MovieCard movie={movie} />
+                    </Link>
+                  ))}
+                </div>
+              ) : (
+                <div className="empty">
+                  <h2>No movies found</h2>
+                </div>
+              )
+            }
+          />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
 
-
-
-
-
+export default App;
 
 
 
--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -1,4 +1,4 @@
-import React from 'react' ; 
+/*import React from 'react' ; 
 
 const MovieCard = ({movie}) => {
 
@@ -20,4 +20,30 @@ const MovieCard = ({movie}) => {
     )
 }
 
-export default MovieCard ; 
+export default MovieCard ; */
+import React from 'react';
+
+const MovieCard = ({ movie }) => {
+  return (
+    <div className="movie">
+      <div>
+        <p>{movie.Year}</p>
+      </div>
+
+      <div>
+        <img
+          src={movie.Poster !== 'N/A' ? movie.Poster : 'https://via.placeholder.com/400'}
+          alt={movie.Title}
+        />
+      </div>
+
+      <div>
+        <span>{movie.Type}</span>
+        <h3>{movie.Title}</h3>
+      </div>
+    </div>
+  );
+};
+
+export default MovieCard;
+
